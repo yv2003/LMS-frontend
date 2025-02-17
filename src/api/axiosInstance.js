1b0f5e3e78a873acdd869ada3bfdb201c65375ca
Yashasvi Vaidya <yashasvivaidya13@gmail.com>
2025-02-17 23:20:16 +0530

    env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: process.env.baseURL
 });
 
 axiosInstance.interceptors.request.use(
