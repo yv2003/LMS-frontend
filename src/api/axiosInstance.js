50cc7918f1a0fd1b43fa3ce12eff9fdab66a213d
Yashasvi Vaidya <yashasvivaidya13@gmail.com>
2025-02-19 23:39:57 +0530

    haha

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-
+// require('dotenv').config();
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL:process.env.PUBLIC_BASE_URL,
+  // baseURL:process.env.PUBLIC_BASE_URL,
+  baseURL:"https://lms-backend-zqvc.onrender.com"
 });
+console.log(axiosInstance.defaults.baseURL);
+// console.log(process.env.PUBLIC_BASE_URL);
 
 axiosInstance.interceptors.request.use(
   (config) => {
